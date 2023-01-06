604d2dfdb4848c4a54aa32ad88cb00719fec726d
Jahdon Ervin <Jahdonervin@gmail.com>
2023-01-06 13:56:52 -0500

    commit tree f98475586b3f57c991f7481147dd526dadf9b292

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i=0; i<word.length/2; i++){
+    const j = word.length - 1 - i
+    if(word[i] !== word[j]) return false
+  }
+  return true
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
